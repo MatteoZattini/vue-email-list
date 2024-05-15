1c09ccb25c48d06b2919e22b369ea29eafbb3b2f
MatteoZattini <matteozattini@hotmail.com>
2024-05-15 17:38:00 +0200

    bonus completi

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ createApp({
   },
   methods: {
     getEmail() {
-      for( let i = 0; i < numberInput.value; i++){
+      for( let i = 0; i < this.myInput; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
           const element = result.data
           this.listaEmail.push(element.response)      
