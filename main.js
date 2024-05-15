64cfb823bf67bd7ceb8b479d72ab6e903117d39c
MatteoZattini <matteozattini@hotmail.com>
2024-05-15 17:14:41 +0200

    update

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ createApp({
     return {
       listaEmail: [],
       myInput: document.getElementById("numberInput"),
+      isLoading: true
     }
   },
   methods: {
@@ -12,9 +13,11 @@ createApp({
       for( let i = 0; i < numberInput.value; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
           const element = result.data
-          this.listaEmail.push(element.response)         
-        });
-      }
+          this.listaEmail.push(element.response)      
+        })
+      };
+
+      this.isLoading = false
     },
 
   },
