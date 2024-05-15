e544992b8c9615191c8e98ad9a72eaf62a1ae1d9
MatteoZattini <matteozattini@hotmail.com>
2024-05-15 16:45:53 +0200

    solo secondo bonus

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      listaEmail: []
-
+      listaEmail: [],
+      myInput: document.getElementById("numberInput"),
     }
   },
   methods: {
     getEmail() {
-      for( let i = 0; i < 10; i++){
+      for( let i = 0; i < numberInput.value; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
           const element = result.data
           this.listaEmail.push(element.response)         
         });
       }
-    }
+    },
 
   },
   mounted() {
