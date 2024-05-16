5459d64aec98de600ddd04e9fa2c9240ba7eb549
MatteoZattini <matteozattini@hotmail.com>
2024-05-16 12:14:24 +0200

    soluzione booleano

--- main.js
+++ main.js
@@ -4,24 +4,33 @@ createApp({
   data() {
     return {
       listaEmail: [],
-      myInput: null
-
+      myInput: null,
+      isLoading: false
     }
   },
   methods: {
     getEmail() {
+      this.isLoading = true
+      this.listaEmail = []
       for( let i = 0; i < this.myInput; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
           const element = result.data
-          this.listaEmail.push(element.response)      
+          this.listaEmail.push(element.response)
+          if( this.listaEmail.length == this.myInput ) {
+            this.isLoading = false
+            console.log("richieste completate")
+          } else {
+            console.log("richieste ma non ultima")
+          }      
         })
-      };
+      }
+      console.log("ciclo completato")
     },
 
   },
   mounted() {
-    this.getEmail();
-    console.log(this.listaEmail)
+    // this.getEmail();
+    // console.log(this.listaEmail)
   },
 
 }).mount('#app')
